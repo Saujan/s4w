9c90a2fbc5d0d91c037d4fd7f64f6b9c81498ce4
Saujan Maka <sauajn@gmail.com>
2020-01-25 13:40:54 +0545

    corrected start_date and end_date

--- src/param_controller.js
+++ src/param_controller.js
@@ -1,3 +1,9 @@
+function appendZero(num) {
+    if (parseInt(num) < 10){
+        return '0'+num
+    }
+    return num
+}
 
 function processDate(period) {
   let startDate, start_date, end_date, endDate = ''
@@ -5,10 +11,10 @@ function processDate(period) {
     return {range: false, date: period.selected}
   else
     startDate = period.startDate.toLocaleString().split(',')[0].split('/')
-    start_date = startDate[2]+'-'+startDate[0]+'-'+startDate[1]
+    start_date = startDate[2] + '-' + appendZero(startDate[0]) + '-' + appendZero(startDate[1])
 
     endDate = period.endDate.toLocaleString().split(',')[0].split('/')
-    end_date = endDate[2]+'-'+endDate[0]+'-'+(Number(endDate[1]) + 1).toString()
+    end_date = endDate[2] + '-' + appendZero(endDate[0]) + '-' + appendZero((Number(endDate[1]) + 1).toString())
     return {range: true, start_date: start_date, end_date: end_date }
 }
 
